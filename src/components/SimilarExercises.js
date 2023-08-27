1b70e59a25bb21ae891faf7581e0ee484819a6aa
Rubaiyat Razin <rubaiyatraha12@gmail.com>
2023-08-27 17:35:24 +0600

    similar exercise part

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Typography, Box, Stack } from '@mui/material';
 
-const SimilarExercises = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import HorizontalScrollbar from './HorizontalScrollbar';
+// import Loader from './';
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
+    <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
+       
+    </Box>
+);
 
 export default SimilarExercises;
